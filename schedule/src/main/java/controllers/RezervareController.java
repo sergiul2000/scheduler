7cc81457cdc421f90ced8bc8d36a639c147318cd
Lupsa Sergiu <sergiul2000@yahoo.com>
2022-05-17 22:11:19 +0300

    commit 5

---
+++ schedule/src/main/java/controllers/RezervareController.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import Services.RezervareService;
+import model.Rezervare;
+import model.Sala;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rezervare")
+public class RezervareController {
+    @Autowired
+    private RezervareService rezervareService;
+
+    public RezervareController(RezervareService rezervareService) {
+        this.rezervareService = rezervareService;
+    }
+
+
+    @GetMapping("/{id}")
+    public Rezervare getRezervareById(@PathVariable Long id){
+        return rezervareService.findById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Rezervare> getRezervare(){
+        return rezervareService.listAll();
+    }
+
+    @PostMapping("/addSala")
+    public Rezervare createRezervare(@RequestBody Rezervare rezervare){
+        return rezervareService.add(rezervare);
+    }
+
+    @PostMapping("/deleteSala/{id}")
+    public void deleteRezervareById(@PathVariable Long id){
+        rezervareService.remove(id);
+    }
+    @PostMapping("/updateSala")
+    public Rezervare updateRezervare(@RequestBody Rezervare rezervare){
+        return rezervareService.update(rezervare);
+    }
+
+
+}
---
+++ schedule/src/main/java/controllers/SalaController.java
@@ -0,0 +1,46 @@
+package controllers;
+
+import Services.SalaService;
+import model.Client;
+import model.Sala;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sala")
+public class SalaController {
+    @Autowired
+    private SalaService salaService;
+
+    public SalaController(SalaService salaService) {
+        this.salaService = salaService;
+    }
+
+    @GetMapping("/{id}")
+    public Sala getSalaById(@PathVariable Long id){
+        return salaService.findById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Sala> getSala(){
+        return salaService.listAll();
+    }
+
+    @PostMapping("/addSala")
+    public Sala createSala(@RequestBody Sala sala){
+        return salaService.add(sala);
+    }
+
+    @PostMapping("/deleteSala/{id}")
+    public void deleteSalaById(@PathVariable Long id){
+        salaService.remove(id);
+    }
+    @PostMapping("/updateSala")
+    public Sala updateSala(@RequestBody Sala sala){
+        return salaService.update(sala);
+    }
+
+
+}
